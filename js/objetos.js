24f05e4eac75147a957e43801706879874b9e2a0
Arthur Molina <gitarthurmolina@gmail.com>
2025-05-19 09:42:47 -0300

    07- adicionadas imagens na pasta Images - Criação do objeto shows -  criação do arquivo montarFront.js - Importação do objeto show

---
+++ js/objetos.js
@@ -0,0 +1,242 @@
+ export const shows = [{
+        artista: "Legião Urbana",
+        local: "São Paulo",
+        data: "2025-06-05",
+        horario: "21:00",
+        tipo_ingressos: "vip",
+        banner: "../images/image0.jpg"
+    },
+    {
+        artista: "Titãs",
+        local: "Rio de Janeiro",
+        data: "2025-06-10",
+        horario: "21:00",
+        tipo_ingressos: "pista",
+        banner: "../images/image1.jpg"
+    },
+    {
+        artista: "Capital Inicial",
+        local: "Belo Horizonte",
+        data: "2025-06-14",
+        horario: "21:00",
+        tipo_ingressos: "premium",
+        banner: "../images/image2.jpg"
+    },
+    {
+        artista: "Paralamas do Sucesso",
+        local: "Curitiba",
+        data: "2025-06-18",
+        horario: "21:00",
+        tipo_ingressos: "meia entrada",
+        banner: "../images/image3.jpg"
+    },
+    {
+        artista: "Engenheiros do Hawaii",
+        local: "Porto Alegre",
+        data: "2025-06-22",
+        horario: "21:00",
+        tipo_ingressos: "inteira",
+        banner: "../images/image4.jpg"
+    },
+    {
+        artista: "Barão Vermelho",
+        local: "Brasília",
+        data: "2025-06-26",
+        horario: "21:00",
+        tipo_ingressos: "vip",
+        banner: "../images/image5.jpg"
+    },
+    {
+        artista: "Os Mutantes",
+        local: "Salvador",
+        data: "2025-06-30",
+        horario: "21:00",
+        tipo_ingressos: "pista",
+        banner: "../images/image6.jpg"
+    },
+    {
+        artista: "Ira!",
+        local: "Fortaleza",
+        data: "2025-07-04",
+        horario: "21:00",
+        tipo_ingressos: "premium",
+        banner: "../images/image7.jpg"
+    },
+    {
+        artista: "Nenhum de Nós",
+        local: "Recife",
+        data: "2025-07-08",
+        horario: "21:00",
+        tipo_ingressos: "meia entrada",
+        banner: "../images/image8.jpg"
+    },
+    {
+        artista: "Skank",
+        local: "Manaus",
+        data: "2025-07-12",
+        horario: "21:00",
+        tipo_ingressos: "inteira",
+        banner: "../images/image9.jpg"
+    },
+    {
+        artista: "Charlie Brown Jr.",
+        local: "Belém",
+        data: "2025-07-16",
+        horario: "21:00",
+        tipo_ingressos: "vip",
+        banner: "../images/image0.jpg"
+    },
+    {
+        artista: "O Rappa",
+        local: "Natal",
+        data: "2025-07-20",
+        horario: "21:00",
+        tipo_ingressos: "pista",
+        banner: "../images/image1.jpg"
+    },
+    {
+        artista: "Pato Fu",
+        local: "João Pessoa",
+        data: "2025-07-24",
+        horario: "21:00",
+        tipo_ingressos: "premium",
+        banner: "../images/image2.jpg"
+    },
+    {
+        artista: "Detonautas",
+        local: "Teresina",
+        data: "2025-07-28",
+        horario: "21:00",
+        tipo_ingressos: "meia entrada",
+        banner: "../images/image3.jpg"
+    },
+    {
+        artista: "Jota Quest",
+        local: "Palmas",
+        data: "2025-08-01",
+        horario: "21:00",
+        tipo_ingressos: "inteira",
+        banner: "../images/image4.jpg"
+    },
+    {
+        artista: "Fresno",
+        local: "Maceió",
+        data: "2025-08-05",
+        horario: "21:00",
+        tipo_ingressos: "vip",
+        banner: "../images/image5.jpg"
+    },
+    {
+        artista: "CPM 22",
+        local: "Aracaju",
+        data: "2025-08-09",
+        horario: "21:00",
+        tipo_ingressos: "pista",
+        banner: "../images/image6.jpg"
+    },
+    {
+        artista: "Pitty",
+        local: "Macapá",
+        data: "2025-08-13",
+        horario: "21:00",
+        tipo_ingressos: "premium",
+        banner: "../images/image7.jpg"
+    },
+    {
+        artista: "Biquini Cavadão",
+        local: "Boa Vista",
+        data: "2025-08-17",
+        horario: "21:00",
+        tipo_ingressos: "meia entrada",
+        banner: "../images/image8.jpg"
+    },
+    {
+        artista: "Lobão",
+        local: "Rio Branco",
+        data: "2025-08-21",
+        horario: "21:00",
+        tipo_ingressos: "inteira",
+        banner: "../images/image9.jpg"
+    },
+    {
+        artista: "Camisa de Vênus",
+        local: "Florianópolis",
+        data: "2025-08-25",
+        horario: "21:00",
+        tipo_ingressos: "vip",
+        banner: "../images/image0.jpg"
+    },
+    {
+        artista: "Matanza",
+        local: "Campo Grande",
+        data: "2025-08-29",
+        horario: "21:00",
+        tipo_ingressos: "pista",
+        banner: "../images/image1.jpg"
+    },
+    {
+        artista: "Raimundos",
+        local: "Cuiabá",
+        data: "2025-09-02",
+        horario: "21:00",
+        tipo_ingressos: "premium",
+        banner: "../images/image2.jpg"
+    },
+    {
+        artista: "Tihuana",
+        local: "Vitória",
+        data: "2025-09-06",
+        horario: "21:00",
+        tipo_ingressos: "meia entrada",
+        banner: "../images/image3.jpg"
+    },
+    {
+        artista: "Dead Fish",
+        local: "Goiânia",
+        data: "2025-09-10",
+        horario: "21:00",
+        tipo_ingressos: "inteira",
+        banner: "../images/image4.jpg"
+    },
+    {
+        artista: "Autoramas",
+        local: "São Luís",
+        data: "2025-09-14",
+        horario: "21:00",
+        tipo_ingressos: "vip",
+        banner: "../images/image5.jpg"
+    },
+    {
+        artista: "Supercombo",
+        local: "Porto Velho",
+        data: "2025-09-18",
+        horario: "21:00",
+        tipo_ingressos: "pista",
+        banner: "../images/image6.jpg"
+    },
+    {
+        artista: "Scalene",
+        local: "Belém",
+        data: "2025-09-22",
+        horario: "21:00",
+        tipo_ingressos: "premium",
+        banner: "../images/image7.jpg"
+    },
+    {
+        artista: "O Surto",
+        local: "Rio de Janeiro",
+        data: "2025-09-26",
+        horario: "21:00",
+        tipo_ingressos: "meia entrada",
+        banner: "../images/image8.jpg"
+    },
+    {
+        artista: "Nação Zumbi",
+        local: "Recife",
+        data: "2025-09-30",
+        horario: "21:00",
+        tipo_ingressos: "inteira",
+        banner: "../images/image9.jpg"
+    }
+];
+  
